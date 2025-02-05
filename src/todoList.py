580e9700f744c7f003db6900f9601d5221ca53f9
Ubuntu <ubuntu@ip-172-31-82-143.ec2.internal>
2025-02-05 19:14:12 +0000

    Fix database issue

--- src/todoList.py
+++ src/todoList.py
@@ -15,7 +15,7 @@ def get_table(dynamodb=None):
             boto3.client = functools.partial(boto3.client, endpoint_url=URL)
             boto3.resource = functools.partial(boto3.resource,
                                                endpoint_url=URL)
-        dynamodb = boto3.resource("dynamodb-dbdbdb")
+        dynamodb = boto3.resource("dynamodb")
     # fetch todo from the database
     table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
     return table
